604c7fae0efd7c4d333b3793a4d7a6ccaaf96b19
GentleTyphoon <minidoras15@gmail.com>
2019-07-16 14:45:08 +0800

    日志记录配置

--- src/test/java/com/imooc/LoggerTest.java
+++ src/test/java/com/imooc/LoggerTest.java
@@ -1,6 +1,7 @@
 package com.imooc;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -10,15 +11,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class LoggerTest {
 
     private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test1() {
-        logger.debug("debug...");
+/*        logger.debug("debug...");
         logger.info("info...");
-        logger.error("error...");
+        logger.error("error...");*/
 
+        /**
+         * @Slf4j
+         */
+        String name = "imooc";
+        String password = "123456";
+
+        log.debug("debug...");
+        log.info("info...");
+        log.error("error...");
+
+        //字符串拼接
+        log.info("name: " + name + ", password: " + password);
+        //占位符
+        log.info("name: {}, password: {}", name, password);
+
+        log.warn("warn...");
     }
 }
