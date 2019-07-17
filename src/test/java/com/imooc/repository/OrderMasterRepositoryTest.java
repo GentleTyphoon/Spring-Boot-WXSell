b5d922f4e13afc526fc4023d52a756f5f0a81ba6
GentleTyphoon <minidoras15@gmail.com>
2019-07-17 19:59:51 +0800

    6-1 买家订单 实体类&DAO

---
+++ src/test/java/com/imooc/repository/OrderMasterRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.imooc.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wwf
+ * @date 2019/7/17 19:43
+ */
+public class OrderMasterRepositoryTest {
+
+    @Test
+    public void findByBuyerOpenid() {
+    }
+}
